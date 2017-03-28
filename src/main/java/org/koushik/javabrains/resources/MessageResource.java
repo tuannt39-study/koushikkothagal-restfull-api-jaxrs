7033d9828f62306f97c6f6c20b4d2dbfc7334813
TUANNT39 <tuannt39@gmail.com>
2017-03-28 16:18:46 +0700

    hello world

---
+++ src/main/java/org/koushik/javabrains/resources/MessageResource.java
@@ -0,0 +1,15 @@
+package org.koushik.javabrains.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/messages")
+public class MessageResource {
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getMessages(){
+        return "Hello World!";
+    }
+}
