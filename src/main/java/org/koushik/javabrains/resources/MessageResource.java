370b4536e103521f3e15d0550ce211b0740c840d
TUANNT39 <tuannt39@gmail.com>
2017-03-31 17:01:50 +0700

    POST method - adding a message

--- src/main/java/org/koushik/javabrains/resources/MessageResource.java
+++ src/main/java/org/koushik/javabrains/resources/MessageResource.java
@@ -1,6 +1,11 @@
 package org.koushik.javabrains.resources;
 
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import java.util.List;
@@ -20,10 +25,11 @@ public class MessageResource {
     }
 
     @POST
+    @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     //http://localhost:8080/webapi/messages/
-    public String addMessage(){
-        return "POST works";
+    public Message addMessage(Message message) {
+        return messageService.addMessage(message);
     }
 
     @GET
