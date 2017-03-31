43da68065b0483ebc8efe2acf165a6e5d4cf578e
TUANNT39 <tuannt39@gmail.com>
2017-03-31 17:14:25 +0700

    PUT method

--- src/main/java/org/koushik/javabrains/resources/MessageResource.java
+++ src/main/java/org/koushik/javabrains/resources/MessageResource.java
@@ -3,6 +3,7 @@ package org.koushik.javabrains.resources;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -14,28 +15,33 @@ import org.koushik.javabrains.model.Message;
 import org.koushik.javabrains.service.MessageService;
 
 @Path("/messages")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+//http://localhost:8080/webapi/messages/
 public class MessageResource {
     MessageService messageService = new MessageService();
 
     @GET
-    //http://localhost:8080/webapi/messages/
-    @Produces(MediaType.APPLICATION_JSON)
     public List<Message> getMessages(){
         return messageService.getAllMessages();
     }
 
     @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    //http://localhost:8080/webapi/messages/
     public Message addMessage(Message message) {
         return messageService.addMessage(message);
+    }
+
+    @PUT
+    @Path("/{messageId}")
+    //http://localhost:8080/webapi/messages/1
+    public Message updateMessage(@PathParam("messageId") long id, Message message) {
+        message.setId(id);
+        return messageService.updateMessage(message);
     }
 
     @GET
     @Path("/{messageId}")
     //http://localhost:8080/webapi/messages/1
-    @Produces(MediaType.APPLICATION_JSON)
     public Message getMessage(@PathParam("messageId") long id) {
         return messageService.getMessage(id);
     }
