9359d2ed6918dfef6ade1e55a5ba3dfe8dc91fda
TUANNT39 <tuannt39@gmail.com>
2017-04-03 16:12:32 +0700

    Comment subresource - mess-id100-comm-id200 test

--- src/main/java/org/koushik/javabrains/resources/CommentResource.java
+++ src/main/java/org/koushik/javabrains/resources/CommentResource.java
@@ -10,4 +10,11 @@ public class CommentResource {
     public String test(){
         return "new sub resource";
     }
+
+    @GET
+    @Path("/{commentId}")
+    //http://localhost:8080/webapi/messages/100/comments/200
+    public String test2(){
+        return "Method to return comment ID";
+    }
 }
