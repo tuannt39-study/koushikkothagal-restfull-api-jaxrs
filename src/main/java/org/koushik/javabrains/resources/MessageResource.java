a8b80d3822b1ed5c7336c646307b0a14a3e75466
TUANNT39 <tuannt39@gmail.com>
2017-04-03 09:44:27 +0700

    Pagination for messages - start&size

--- src/main/java/org/koushik/javabrains/resources/MessageResource.java
+++ src/main/java/org/koushik/javabrains/resources/MessageResource.java
@@ -24,9 +24,14 @@ public class MessageResource {
     MessageService messageService = new MessageService();
 
     @GET
-    public List<Message> getMessages(@QueryParam("year") int year) {
+    public List<Message> getMessages(@QueryParam("year") int year,
+                                     @QueryParam("start") int start,
+                                     @QueryParam("size") int size) {
         if (year > 0) {
             return messageService.getAllMessagesForYear(year);
+        }
+        if (start >= 0 && size > 0) {
+            return messageService.getAllMessagesPaginated(start, size);
         }
         return messageService.getAllMessages();
     }
