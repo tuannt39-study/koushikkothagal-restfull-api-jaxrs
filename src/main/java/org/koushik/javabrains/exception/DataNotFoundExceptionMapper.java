fd56d069deb621f56e1fe0e0084a7de3984b4e77
TUANNT39 <tuannt39@gmail.com>
2017-04-03 17:54:52 +0700

    Handling Exceptions - Error 404

--- src/main/java/org/koushik/javabrains/exception/DataNotFoundExceptionMapper.java
+++ src/main/java/org/koushik/javabrains/exception/DataNotFoundExceptionMapper.java
@@ -5,11 +5,16 @@ import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
+import org.koushik.javabrains.model.ErrorMessage;
+
 @Provider
 public class DataNotFoundExceptionMapper implements ExceptionMapper<DataNotFoundException> {
     @Override
     //http://localhost:8080/webapi/messages/100
     public Response toResponse(DataNotFoundException ex) {
-        return Response.status(Status.NOT_FOUND).build();
+        ErrorMessage errorMessage = new ErrorMessage(ex.getMessage(), 404, "http://qa.hackademicshanoi.vn/");
+        return Response.status(Status.NOT_FOUND)
+                .entity(errorMessage)
+                .build();
     }
 }
