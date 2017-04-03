1d15f8b17876766e9cf34d6243f04a3d0cd16328
TUANNT39 <tuannt39@gmail.com>
2017-04-03 18:12:54 +0700

    Using WebApplicationException

--- src/main/java/org/koushik/javabrains/service/CommentService.java
+++ src/main/java/org/koushik/javabrains/service/CommentService.java
@@ -4,8 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
 import org.koushik.javabrains.database.DatabaseClass;
 import org.koushik.javabrains.model.Comment;
+import org.koushik.javabrains.model.ErrorMessage;
 import org.koushik.javabrains.model.Message;
 
 public class CommentService {
@@ -17,9 +23,24 @@ public class CommentService {
         return new ArrayList<Comment>(comments.values());
     }
 
+    //http://localhost:8080/webapi/messages/100/comments/1
     public Comment getComment(long messageId, long commentId) {
+        ErrorMessage errorMessage = new ErrorMessage("Not found", 404, "http://qa.hackademicshanoi.vn/");
+        Response response = Response.status(Status.NOT_FOUND)
+                .entity(errorMessage)
+                .build();
+
+        Message message = messages.get(messageId);
+        if (message == null) {
+            throw new WebApplicationException(response);
+        }
         Map<Long, Comment> comments = messages.get(messageId).getComments();
-        return comments.get(commentId);
+        Comment comment = comments.get(commentId);
+        if (comment == null) {
+            //throw new WebApplicationException(response);
+            throw new NotFoundException(response);
+        }
+        return comment;
     }
 
     public Comment addComment(long messageId, Comment comment) {
@@ -42,5 +63,4 @@ public class CommentService {
         Map<Long, Comment> comments = messages.get(messageId).getComments();
         return comments.remove(commentId);
     }
-
 }
