8a8dd79b2ed871db9d3ca540a09bda8d960d89ff
TUANNT39 <tuannt39@gmail.com>
2017-04-03 10:03:57 +0700

    Param annotations - test

---
+++ src/main/java/org/koushik/javabrains/resources/InjectDemoResource.java
@@ -0,0 +1,19 @@
+package org.koushik.javabrains.resources;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/injectdemo")
+@Consumes(MediaType.TEXT_PLAIN)
+@Produces(MediaType.TEXT_PLAIN)
+public class InjectDemoResource {
+
+    @GET
+    @Path("annotations")
+    public String getParamsUsingAnnotations() {
+        return "test";
+    }
+}
