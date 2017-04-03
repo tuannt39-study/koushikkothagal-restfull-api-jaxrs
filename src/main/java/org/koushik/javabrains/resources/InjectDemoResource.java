2b1e8f1dd8cea9635f2319963a8afa9346eab71d
TUANNT39 <tuannt39@gmail.com>
2017-04-03 10:58:54 +0700

    Context annotations - Path

--- src/main/java/org/koushik/javabrains/resources/InjectDemoResource.java
+++ src/main/java/org/koushik/javabrains/resources/InjectDemoResource.java
@@ -7,18 +7,30 @@ import javax.ws.rs.HeaderParam;
 import javax.ws.rs.MatrixParam;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
 
 @Path("/injectdemo")
 @Consumes(MediaType.TEXT_PLAIN)
 @Produces(MediaType.TEXT_PLAIN)
+//http://localhost:8080/webapi/injectdemo
 public class InjectDemoResource {
 
     @GET
     @Path("annotations")
+    //http://localhost:8080/webapi/injectdemo/annotations
     public String getParamsUsingAnnotations(@MatrixParam("param") String matrixParam,
                                             @HeaderParam("authSessionID") String header,
                                             @CookieParam("name") String cookie) {
         return "Matrix param = " + matrixParam + " - Header param = " + header + " - Cookie param: " + cookie;
+    }
+
+    @GET
+    @Path("context")
+    //http://localhost:8080/webapi/injectdemo/context
+    public String getParamsUsingContext(@Context UriInfo uriInfo) {
+        String path = uriInfo.getAbsolutePath().toString();
+        return "Path = " + path;
     }
 }
